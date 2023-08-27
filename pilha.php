4f6f4b467325726d8117a1c98037e0bacaa4e09e
Eric Araújo Lima <eric.araujolima@hotmail.com>
2023-08-27 15:59:24 -0300

    Utilizando Try Catch

--- pilha.php
+++ pilha.php
@@ -3,13 +3,25 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+    try {
+        funcao2();
+    } catch (\RuntimeException | \DivisionByZeroError  $erro)  {
+        echo $erro->getMessage() . PHP_EOL;
+        echo $erro->getLine() . PHP_EOL;
+        echo $erro->getTraceAsString() . PHP_EOL;
+    }
+
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+    
+    $arrayFixo = new SplFixedArray(2)    ;
+    $arrayFixo[3] = 'Valor';
+    $divisao = intdiv(5, 0);
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
