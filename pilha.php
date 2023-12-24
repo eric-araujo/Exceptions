191e099481d1cf728cb858db2b39582ea312c03e
Eric Araújo Lima <eric.araujolima@hotmail.com>
2023-12-24 15:25:24 -0300

    Entendendo Exceções

--- pilha.php
+++ pilha.php
@@ -1,14 +1,20 @@
 <?php
 
+/**
+ * @throws DivisionByZeroError
+ * @throws RuntimeException
+ */
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
     try {
         funcao2();
-    } catch (\RuntimeException | \DivisionByZeroError  $erro)  {
+    } catch (RuntimeException | DivisionByZeroError  $erro) {
         echo $erro->getMessage() . PHP_EOL;
         echo $erro->getLine() . PHP_EOL;
         echo $erro->getTraceAsString() . PHP_EOL;
+
+        throw new RuntimeException('Erro tratado');
     }
 
     echo 'Saindo da função 1' . PHP_EOL;
@@ -17,15 +23,8 @@ function funcao1()
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
-    
-    $arrayFixo = new SplFixedArray(2)    ;
-    $arrayFixo[3] = 'Valor';
-    $divisao = intdiv(5, 0);
 
-    for ($i = 1; $i <= 5; $i++) {
-        echo $i . PHP_EOL;
-    }
-    echo 'Saindo da função 2' . PHP_EOL;
+    throw new RuntimeException('', 0, new RuntimeException());
 }
 
 echo 'Iniciando o programa principal' . PHP_EOL;
